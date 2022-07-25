b2b8d0735db1f06a000d6f1fa6fd64f96e3417f3
Tomohiko Ozawa <kota65535@gmail.com>
2022-07-26 02:25:39 +0900

    fix doc

--- examples/resources/alternator_database_schema/resource.tf
+++ examples/resources/alternator_database_schema/resource.tf
@@ -1,19 +1,20 @@
 resource "alternator_database_schema" "example" {
   database = "example"
   schema   = <<EOF
-CREATE DATABASE example
-    DEFAULT CHARACTER SET utf8mb4
-    DEFAULT COLLATE utf8mb4_0900_bin;
-
+CREATE DATABASE example;
 USE example;
-
-CREATE TABLE greeting
+CREATE TABLE users
 (
-    id         int AUTO_INCREMENT,
-    body       varchar(255),
-    created_at datetime DEFAULT CURRENT_TIMESTAMP,
-    updated_at datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-    PRIMARY KEY (id)
+    id   int PRIMARY KEY,
+    name varchar(100)
+);
+CREATE TABLE blog_posts
+(
+    id        int PRIMARY KEY,
+    title     varchar(100),
+    body      text,
+    author_id int,
+    FOREIGN KEY (author_id) REFERENCES users (id)
 );
 EOF
 }
